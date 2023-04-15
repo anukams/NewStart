75c2821fb15ceb712d9487e94477ce04e4c6cc15
Anukam <anukams@amazon.com>
2023-04-15 05:59:24 -0400

    initial commit

---
+++ src/main/java/com/example/NewSatrt/NewSatrtApplication.java
@@ -0,0 +1,13 @@
+package com.example.NewSatrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSatrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSatrtApplication.class, args);
+	}
+
+}
